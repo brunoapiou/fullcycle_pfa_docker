3c8b65cf1aeb35cf7190d0abbddcd86e91bddd59
Bruno Godoy <bruno_apiou@hotmail.com>
2021-04-24 23:02:45 -0300

    Added sh file to run everything

---
+++ execute.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#NETWORK
+docker network create --driver bridge minharede
+
+#DB
+cd db
+
+docker build -t mysql_image .
+docker run --rm -d --network minharede --name mysql mysql_image
+cd ..
+
+#BACK-END
+cd app
+
+docker build -t myapp_image .
+docker run --rm -d --network minharede --name myapp -p 5000:5000 myapp_image
+cd ..
+
+#NGINX
+cd web
+
+docker build -t mynginx_image .
+docker run --rm -d --network minharede --name mynginx -p 80:80 mynginx_image
