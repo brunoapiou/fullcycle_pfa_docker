3b4e54cb994732758568652d928f70700775b4e6
Bruno Godoy <bruno_apiou@hotmail.com>
2021-04-25 21:56:49 -0300

    fix execute to reflect what the chalange request

--- execute.sh
+++ execute.sh
@@ -3,21 +3,10 @@
 docker network create --driver bridge minharede
 
 #DB
-cd db
-
-docker build -t mysql_image .
-docker run --rm -d --network minharede --name mysql mysql_image
-cd ..
+docker run --rm -d --network minharede --name mysql brunoapiou/mysql_pfa
 
 #BACK-END
-cd app
-
-docker build -t myapp_image .
-docker run --rm -d --network minharede --name myapp -p 5000:5000 myapp_image
-cd ..
+docker run --rm -d --network minharede --name myapp brunoapiou/app_pfa
 
 #NGINX
-cd web
-
-docker build -t mynginx_image .
-docker run --rm -d --network minharede --name mynginx -p 8080:8080 mynginx_image
+docker run --rm -d --network minharede --name mynginx -p 8080:8080 brunoapiou/nginx_pfa
